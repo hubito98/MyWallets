f2f9ca0c9393163be8c6815e61c1b706c41f8228
hubito98 <hskrzypczak98@gmail.com>
2021-11-11 16:26:05 +0100

    Set specific packages version in backend Dockerfile to avoid regressions

--- backend/setup_json.sh
+++ backend/setup_json.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/nlohmann/json.git
+cd json && git checkout e9f88c2fad4b910b90262019df72181c14a0370f && cd ..
 mv json/single_include/nlohmann .
 rm -rf json
--- backend/setup_served.sh
+++ backend/setup_served.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/meltwater/served.git
+cd served && git checkout 2eb36b83fa69330900ae58ef41d12b5f7abc138c && cd ..
 mkdir served.build && cd served.build
 cmake ../served && make
 make install
