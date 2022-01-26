56989ccdc169fc6325e8d1a8574aba441f8ad580
hubito98 <hskrzypczak98@gmail.com>
2022-01-26 12:26:22 +0100

    Add microservice for calculating wallet timeline

---
+++ wallettimeline/src/test/java/com/hubert/wallettimeline/WallettimelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubert.wallettimeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallettimelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
